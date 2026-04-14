c5555ebb0e872848f4fe53782fd23038a7a66cb7
zzx <123@email.com>
2026-04-14 17:32:22 +0800

    添加玩家状态类

---
+++ TileMind/TileMind.Common/Models/PlayerTileState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TileMind.Common.Models
+{
+    public class PlayerTileState
+    {
+        //手牌状态
+        public List<TileDetectionResult> HandTiles { get; }
+        //牌河
+        public List<TileDetectionResult> DiscardPondTiles { get; }
+        //副露
+        public List<TileDetectionResult> MeldTiles { get; }
+        //暗杠
+        public List<TileDetectionResult> AnkanTiles { get; }
+    }
+}
