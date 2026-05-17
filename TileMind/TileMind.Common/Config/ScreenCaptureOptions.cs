f9b499d66d23b5316be1adc9cabee7d59b49e98a
zzijin <zzx1248022549@126.com>
2026-05-17 21:37:46 +0800

    修正依赖结构，将ScreenCaptureOptions中的计算属性的计算逻辑从ScreenSplitterOverlayControl中剔除

--- TileMind/TileMind.Common/Config/ScreenCaptureOptions.cs
+++ TileMind/TileMind.Common/Config/ScreenCaptureOptions.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using TileMind.Common.Helpers;
 
 namespace TileMind.Common.Config
 {
@@ -57,7 +58,37 @@ namespace TileMind.Common.Config
         [JsonIgnore]
         public Point[] LeftDiscardPondArea { get; set; } = new Point[4];
 
-        /// <summary>从另一实例复制所有区域值（用于 Reload 时原地更新单例）。</summary>
+        /// <summary>
+        /// 使用四个基础区域（TableArea、DiscardPondArea、InfoArea）计算所有八个派生区域。
+        /// 在 JSON 反序列化后、基础区域变更后、或 CopyFrom 后调用。
+        /// </summary>
+        public void ComputeDerivedAreas()
+        {
+            if (TableArea.Length != 4 || DiscardPondArea.Length != 4 || InfoArea.Length != 4)
+                return;
+
+            var B = DiscardPondArea;
+            var D = InfoArea;
+
+            // 弃牌区：QuadB 与 QuadD 之间的梯形
+            SelfDiscardPondArea = new[] { B[3], D[3], D[2], B[2] };
+            RightDiscardPondArea = new[] { B[2], D[2], D[1], B[1] };
+            OppositeDiscardPondArea = new[] { B[1], D[1], D[0], B[0] };
+            LeftDiscardPondArea = new[] { B[0], D[0], D[3], B[3] };
+
+            // 手牌+副露区：从 QuadB 各边向外延伸与 TableArea 边界求交
+            var intersectA = GeometryHelper.FindRayBoundaryIntersection(B, TableArea, 1, 0);
+            var intersectB = GeometryHelper.FindRayBoundaryIntersection(B, TableArea, 2, 1);
+            var intersectC = GeometryHelper.FindRayBoundaryIntersection(B, TableArea, 3, 2);
+            var intersectD = GeometryHelper.FindRayBoundaryIntersection(B, TableArea, 0, 3);
+
+            LeftHandAndMeldArea = new[] { intersectA, B[0], B[3], intersectD };
+            OppositeHandAndMeldArea = new[] { intersectB, B[1], B[0], intersectA };
+            RightHandAndMeldArea = new[] { intersectC, B[2], B[1], intersectB };
+            SelfHandAndMeldArea = new[] { intersectD, B[3], B[2], intersectC };
+        }
+
+        /// <summary>从另一实例复制基础配置值（用于 Reload 时原地更新单例）。</summary>
         public void CopyFrom(ScreenCaptureOptions other)
         {
             AdapterIndex = other.AdapterIndex;
@@ -66,14 +97,7 @@ namespace TileMind.Common.Config
             TableArea = other.TableArea;
             DiscardPondArea = other.DiscardPondArea;
             InfoArea = other.InfoArea;
-            SelfHandAndMeldArea = other.SelfHandAndMeldArea;
-            SelfDiscardPondArea = other.SelfDiscardPondArea;
-            RightHandAndMeldArea = other.RightHandAndMeldArea;
-            RightDiscardPondArea = other.RightDiscardPondArea;
-            OppositeHandAndMeldArea = other.OppositeHandAndMeldArea;
-            OppositeDiscardPondArea = other.OppositeDiscardPondArea;
-            LeftHandAndMeldArea = other.LeftHandAndMeldArea;
-            LeftDiscardPondArea = other.LeftDiscardPondArea;
+            ComputeDerivedAreas();
         }
     }
 }
